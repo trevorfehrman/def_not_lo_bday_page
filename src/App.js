3fcc286113634df11851b87d66e1c4352e401422
Trevor Fehrman <trevorfehrman@gmail.com>
2019-05-06 16:51:03 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import ReactHowler from 'react-howler';
+
+import jungle from './assets/jungle.jpeg';
+import lo from './assets/lilblue.png';
+import me from './assets/me.png';
+import message from './assets/bdaytext.png';
+import music from './assets/Jurassic Park - Main Theme.mp3';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url(${jungle});
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+`;
+
+const Inner = styled.div`
+  display: flex;
+
+  .trevor {
+    width: 75%;
+    height: 75%;
+    position: absolute;
+    right: 2rem;
+    bottom: 0;
+  }
+
+  .lo {
+    position: absolute;
+    left: 4rem;
+    bottom: 0;
+  }
+
+  .message {
+    position: relative;
+    top: -40rem;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Inner>
+        <img className='message' src={message} />
+        <img className='lo' src={lo} />
+        <img className='trevor' src={me} />
+      </Inner>
+      <ReactHowler src={music} playing={true} loop={true} />
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+
+
+const theme = {
+  white: '#fafafa'
+};
+
+
+const GlobalStyle = createGlobalStyle`
+
+  *,
+  *::after,
+  *::before{
+    margin: 0;
+    padding: 0;
+    box-sizing:inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+      font-size: 1.8rem;
+  }
+
+`;
+
+ReactDOM.render(
+  <Fragment>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <GlobalStyle />
+        <App />
+      </Fragment>
+    </ThemeProvider>
+  </Fragment>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
